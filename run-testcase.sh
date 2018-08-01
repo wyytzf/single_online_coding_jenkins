733e4b9d8acdd0129297d59c4fae5601b0d89967
wyytzf <wyytzf@163.com>
2018-08-01 16:28:21 +0800

    Update run-testcase.sh

--- run-testcase.sh
+++ run-testcase.sh
@@ -2,9 +2,10 @@
 echo $test_data > test_data.json
 
 COUNT=$(cat test_data.json| jq -r ".[] | length")
-echo "COUNT : $COUNT"
+echo "COUNT: $COUNT"
 for k in $( seq 0 $COUNT ); do
   idx=`expr $k`
+  echo "times:$idx"
   PARAM=$(cat test_data.json| jq -r ".[$idx].input")
   ACTUAL=$(sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo $PARAM")
   EXCEPT=$(cat test_data.json| jq -r ".[$idx].expectedOutput")
