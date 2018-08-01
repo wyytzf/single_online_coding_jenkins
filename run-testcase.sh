a28f811fd39ade2ed0573612d2d80f5790392990
wyytzf <wyytzf@163.com>
2018-08-01 16:46:11 +0800

    Update run-testcase.sh

--- run-testcase.sh
+++ run-testcase.sh
@@ -5,7 +5,7 @@ COUNT=$(cat test_data.json| jq -r ".[] | length")
 echo "COUNT: $COUNT"
 for k in $( seq 1 $COUNT );
 do
-  idx=`expr $k-1`
+  idx=`expr $k - 1`
   echo "times:$idx"
   PARAM=$(cat test_data.json| jq -r ".[$idx].input")
   ACTUAL=$(sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo $PARAM")
