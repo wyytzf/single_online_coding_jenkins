325e3572fd1bc090630ec59f510976921904db96
wyytzf <wyytzf@163.com>
2018-08-01 10:55:17 +0800

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo docker run --tty --name testdocker --detach $image
+sudo docker run --tty --name javadocker --detach $image
 sudo docker ps -a
 # sudo docker cp ./answer testdocker:/var/test_directory
 # sudo docker exec -i testdocker /bin/sh -c "ls /var/test_directory"
