617cf2dd74f544008125d9f9a69a877ee2502165
wyytzf <wyytzf@163.com>
2018-08-01 16:58:56 +0800

    Update run-testcase.sh

--- run-testcase.sh
+++ run-testcase.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
-echo $test_data > test_data.json
+  #!/bin/sh
+  echo $test_data > test_data.json
 
-COUNT=$(cat test_data.json| jq -r ".[] | length")
-COUNT=`expr $COUNT + 1`
-for k in $( seq 1 $COUNT );
-do
-  idx=`expr $k - 1`
-  echo "times:$idx"
-  echo "k:$k"
-  PARAM=$(cat test_data.json| jq -r ".[$idx].input")
-  ACTUAL=$(sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo $PARAM")
-  EXCEPT=$(cat test_data.json| jq -r ".[$idx].expectedOutput")
+  COUNT=$(cat test_data.json| jq -r ".[] | length")
+  echo "COUNT:$COUNT"
+  for k in $( seq 1 $COUNT );
+  do
+    idx=`expr $k - 1`
+    echo "times:$idx"
+    echo "k:$k"
+    PARAM=$(cat test_data.json| jq -r ".[$idx].input")
+    ACTUAL=$(sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo $PARAM")
+    EXCEPT=$(cat test_data.json| jq -r ".[$idx].expectedOutput")
 
-  echo "期望: $EXCEPT"
-  echo "实际: $ACTUAL"
-done
+    echo "期望: $EXCEPT"
+    echo "实际: $ACTUAL"
+  done
 
-#sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo ThoughWorks"
+  #sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo ThoughWorks"
