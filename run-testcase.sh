4f12e0cb60e95d1cb0fba209a99f5a06b3b3db6b
wyytzf <wyytzf@163.com>
2018-08-01 16:14:43 +0800

    Add files via upload

--- run-testcase.sh
+++ run-testcase.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-echo $test_data
-sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo ThoughtWorks"
+echo $test_data > test_data.json
+
+COUNT=$(cat test_data.json| jq -r ".test | length")
+
+for k in $( seq 1 $COUNT ); do
+  idx=`expr $k - 1`
+  PARAM=$(cat test_data.json| jq -r ".test[$idx].in")
+  ACTUAL=$(sudo docker exec -it javadocker sh -c "cd /var/test_directory && java demo $PARAM")
+  EXCEPT=$(cat test_data.json| jq -r ".test[$idx].out")
+
+  echo "期望: $EXCEPT"
+  echo "实际: $ACTUAL"
+done
+
+#sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo ThoughWorks"
