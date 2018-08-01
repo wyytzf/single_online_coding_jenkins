9b1f3f23deb99e0207f480dce025cdea08fd682a
wyytzf <wyytzf@163.com>
2018-08-01 11:10:27 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo $user_answer_code >> demo.java
+sudo docker exec -i javadocker sh -c "mkdir /var/test_directory/"
 sudo docker cp demo.java javadocker:/var/test_directory/demo.java
 sudo docker exec -i javadocker sh -c "cd /var/test_directory && javac -d demo.java"
