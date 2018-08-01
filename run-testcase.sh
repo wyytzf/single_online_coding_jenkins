3ddd4114c02843aeb81c7a91358f5d7f1faec662
wyytzf <wyytzf@163.com>
2018-08-01 17:22:51 +0800

    Update run-testcase.sh

--- run-testcase.sh
+++ run-testcase.sh
@@ -10,15 +10,16 @@ for k in $( seq 1 $COUNT );
     ACTUAL=$(sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo $PARAM")
     EXCEPT=$(cat test_data.json| jq -r ".[$idx].expectedOutput")
 
-    echo "期望: $EXCEPT"
-    echo "实际: $ACTUAL"
+    echo "期望: $EXCEPT" >> result
+    echo "\n" >> result
+    echo "实际: $ACTUAL\n" >> result
     if [[ "$ACTUAL" == "$EXCEPT" ]];
     then
         let "SUCCESS_COUNT += 1"
-    else
-        let "SUCCESS_COUNT += 2"
     fi
-done
-echo "SUCCESS_COUNT:$SUCCESS_COUNT"
 
+done
+echo "SUCCESS_COUNT:$SUCCESS_COUNT\n" >> result
+echo "TOTAL_COUNT:$COUNT" >> result
+cat result
     #sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo ThoughWorks"
