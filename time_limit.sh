8046030e1c45cd8162bcfc15ed59c86fff4e3f03
wyytzf <wyytzf@163.com>
2018-08-02 16:39:22 +0800

    Update time_limit.sh

--- time_limit.sh
+++ time_limit.sh
@@ -3,7 +3,7 @@ seconds_left=10
 while [ $seconds_left -gt 0 ];do
   sleep 1
   seconds_left=$(($seconds_left - 1))
-  count=`ps -ef | grep $1 | grep -v "grep" | wc -l`
+  count=`ps -ef | grep $1 | grep -v $0 | wc -l`
   echo $count
   if [ "$(ps ef|grep $1)" == "" ];
   then
