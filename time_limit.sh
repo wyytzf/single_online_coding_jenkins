226030feebf851ea8013d29e6371b7d7bf1b1d7a
wyytzf <wyytzf@163.com>
2018-08-02 16:27:22 +0800

    Update time_limit.sh

--- time_limit.sh
+++ time_limit.sh
@@ -3,6 +3,7 @@ seconds_left=10
 while [ $seconds_left -gt 0 ];do
   sleep 1
   seconds_left=$(($seconds_left - 1))
+  echo "=====   $(ps ef|grep '$1')"
   if [ "$(ps ef|grep '$1')" == "" ];
   then
     echo "成功"
