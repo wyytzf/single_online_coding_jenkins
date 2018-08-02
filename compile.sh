27a5b8de809e1b878b3dab2dffe2e6e16c990f49
wyytzf <wyytzf@163.com>
2018-08-02 16:51:12 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ echo $user_answer_code > demo.java
 sudo docker exec -i javadocker sh -c "mkdir /var/test_directory/"
 sudo docker cp demo.java javadocker:/var/test_directory/demo.java
 STARTTIME=`date +%s%N`
-sudo docker exec -i javadocker sh -c "cd /var/test_directory && javac -verbose demo.java" 1>result-compile 2>&1
+sudo docker exec -i javadocker sh -c "cd /var/test_directory && javac -verbose demo.java" 1>compile-success 2>compile-error
 ENDTIME=`date +%s%N`
 COSTTIME=`expr $ENDTIME - $STARTTIME`
 echo "编译时间:$COSTTIME" >> result
