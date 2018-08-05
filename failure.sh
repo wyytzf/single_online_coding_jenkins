8bc468c87f184047883256c3cb71477e1774ef65
wyytzf <wyytzf@163.com>
2018-08-05 13:02:07 +0800

    Add files via upload

--- failure.sh
+++ failure.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 job_msg=$(cat online-coding-master/result)
+# status 1-编译超时 2-编译错误 3-运行超时
+# 这里有个问题，当运行超时时，job_msg不应该有内容
 curl -X POST \
     -F "status=$STATUS" \
     -F "msg=$LOCAL_ERROR" \
--- run_testcase.sh
+++ run_testcase.sh
@@ -2,7 +2,7 @@
 echo $test_data > test_data.json
 
 COUNT=$(cat test_data.json| jq -r ". | length")
-SUCCESS_COUNT=0
+#SUCCESS_COUNT=0
 STARTTIME=`date +%s%N`
 for k in $( seq 1 $COUNT );
   do
@@ -11,15 +11,18 @@ for k in $( seq 1 $COUNT );
     ACTUAL=$(sudo docker exec -i javadocker sh -c "cd /var/test_directory && java demo $PARAM")
     EXCEPT=$(cat test_data.json| jq -r ".[$idx].expectedOutput")
 
-    echo "期望: $EXCEPT" >> result
-    echo "实际: $ACTUAL" >> result
-    if [[ "$ACTUAL" == "$EXCEPT" ]];
+  #  echo "期望: $EXCEPT" >> result
+  #  echo "实际: $ACTUAL" >> result
+    if [[ "$ACTUAL" != "$EXCEPT" ]];
     then
-        let "SUCCESS_COUNT += 1"
+        echo -e "期望\n: $EXCEPT" > result
+        echo -e "实际: $ACTUAL" >> result
+        exit 1
+        #let "SUCCESS_COUNT += 1"
     fi
 done
 ENDTIME=`date +%s%N`
 COSTTIME=`expr $ENDTIME - $STARTTIME`
-echo "SUCCESS_COUNT:$SUCCESS_COUNT" >> result
-echo "TOTAL_COUNT:$COUNT" >> result
+#echo "SUCCESS_COUNT:$SUCCESS_COUNT" >> result
+#echo "TOTAL_COUNT:$COUNT" >> result
 echo "运行时间:$COSTTIME" >> result
