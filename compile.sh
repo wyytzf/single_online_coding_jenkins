836b35eac8e60ebb86f20fdea8014f576fd52816
wyytzf <wyytzf@163.com>
2018-08-07 14:55:36 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -6,5 +6,5 @@ STARTTIME=`date +%s%N`
 sudo docker exec -i javadocker sh -c "cd /var/test_directory && javac demo.java" 1>compile-success 2>compile-error
 ENDTIME=`date +%s%N`
 COSTTIME=`expr $ENDTIME - $STARTTIME`
-echo "编译时间:$COSTTIME" >> result
+echo "编译时间:$COSTTIME" > result
 exit 0
