d3c2022ddcfa8e32858ba632900406ab2d11b8ff
wyytzf <wyytzf@163.com>
2018-08-07 10:46:07 +0800

    Add files via upload

--- failure.sh
+++ failure.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 job_msg=$(cat online-coding-master/result)
-# status 1-编译超时 2-编译错误 3-运行超时
+# status 1-编译超时 2-编译错误 3-运行超时 4-运行错误
 # 这里有个问题，当运行超时时，job_msg不应该有内容
 curl -X POST \
-    -F "status=$STATUS" \
+    -F "status=0" \
     -F "msg=$LOCAL_ERROR" \
     -F "job_msg=$job_msg" \
     $callback_url
--- success.sh
+++ success.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 job_msg=$(cat online-coding-master/result)
 curl -X POST \
-    -F "status=4" \
+    -F "status=2" \
     -F "msg=运行成功" \
     -F "job_msg=$job_msg" \
     $callback_url
