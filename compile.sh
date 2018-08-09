efe4dc2cba574399d06ebf0d6e24dfe44b9a5a04
wyytzf <wyytzf@163.com>
2018-08-09 10:04:41 +0800

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo "${user_answer_code}" > demo.java
+echo "${user_answer_code}" > demo.${postfix}
 sudo docker exec -i $language sh -c "mkdir /var/test_directory/"
-sudo docker cp demo.java $language:/var/test_directory/demo.java
+sudo docker cp demo.${postfix} $language:/var/test_directory/demo.${postfix}
 STARTTIME=`date +%s%N`
-sudo docker exec -i $language sh -c "cd /var/test_directory && javac demo.java" 1>compile-success 2>compile-error
+sudo docker exec -i $language sh -c "cd /var/test_directory && ${compile} demo.java" 1>compile-success 2>compile-error
 ENDTIME=`date +%s%N`
 COSTTIME=`expr $ENDTIME - $STARTTIME`
 echo "编译时间:$COSTTIME" > result
