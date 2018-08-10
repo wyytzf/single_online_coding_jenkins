fbcdb34f9c6cf3812506eadf8aa830701042023f
wyytzf <wyytzf@163.com>
2018-08-10 17:16:56 +0800

    Add files via upload

--- build-image.sh
+++ build-image.sh
@@ -2,5 +2,7 @@
 sudo docker run --tty --name $language --detach $image /bin/sh
 sudo docker ps -a
 sudo docker exec -i $language sh -c "mkdir /var/test_directory/"
+echo "${user_answer_code}" > demo${sourcePostfix}
+sudo docker cp demo${sourcePostfix} $language:/var/test_directory/demo${sourcePostfix}
 # sudo docker cp ./answer testdocker:/var/test_directory
 # sudo docker exec -i testdocker /bin/sh -c "ls /var/test_directory"
--- compile.sh
+++ compile.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-echo "dads"
-echo "${user_answer_code}" > demo${sourcePostfix}
-sudo docker cp demo${sourcePostfix} $language:/var/test_directory/demo${sourcePostfix}
 STARTTIME=`date +%s%N`
 sudo docker exec -i $language sh -c "cd /var/test_directory && ${compile} demo${sourcePostfix}" 1>compile-success 2>compile-error
 ENDTIME=`date +%s%N`
