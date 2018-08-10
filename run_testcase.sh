2f29b1546fac48b47f0f4f31fb2b3e342156899b
wyytzf <wyytzf@163.com>
2018-08-10 17:10:38 +0800

    Update run_testcase.sh

--- run_testcase.sh
+++ run_testcase.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 echo "${test_data}" > test_data.json
-
+echo 1
 COUNT=$(cat test_data.json| jq -r ". | length")
 #SUCCESS_COUNT=0
 STARTTIME=`date +%s%N`
+echo 2
 for k in $( seq 1 $COUNT );
+  echo 3
   do
+    echo 4
     idx=`expr $k - 1`
+    echo 5
     PARAM=$(cat test_data.json| jq -r ".[$idx].input")
+    echo 6
     ACTUAL=$(sudo docker exec -i $language sh -c "cd /var/test_directory && ${execute} demo${executePostfix} $PARAM")
+    echo 7
     EXCEPT=$(cat test_data.json| jq -r ".[$idx].expectedOutput")
 
   #  echo "期望: $EXCEPT" >> result
